bb0b1c99f5289251945a98eee70e750c524fec7d
jay-kalariya <jaykalariya1209@gmail.com>
2024-01-29 17:15:14 +0530

    new updates

--- interiormaata/src/app/Home/page.jsx
+++ interiormaata/src/app/Home/page.jsx
@@ -1,16 +1,13 @@
 "use client"
 import React from 'react'
 import Stairs from "@/Animations/Stairs"
-import RT_Circle from '@/Components/RT_Circle/page'
 const Page = () => {
  
   return (
     <div>
       <Stairs>
         <h1>Home</h1>
-
       </Stairs>
-      <RT_Circle />
     </div>
   )
 }
