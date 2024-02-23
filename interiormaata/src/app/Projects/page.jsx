d442768254386e6939556dcefb5e57768169af39
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-02-23 13:17:43 +0530

    upd -project -j

--- interiormaata/src/app/Projects/page.jsx
+++ interiormaata/src/app/Projects/page.jsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import Stairs from "@/Animations/Stairs"
 import ProjectPage from "@/Components/ProjectPage/page"
+import Project_Flex from "@/Components/Projects_Flex/page"
 const Page = () => {
   return (
     <div>
     <Stairs>
       <ProjectPage/>
+      <Project_Flex />  
     </Stairs>
     </div>
   )
